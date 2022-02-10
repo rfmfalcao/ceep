f46f3994550983557a1eb742599c14dcd856b237
rfmfalcao <rfmfalcao@gmail.com>
2022-02-10 12:29:31 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { ListaDeNotas } from './components/ListaDeNotas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
   );
 }
 
---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export class ListaDeNotas extends Component {
+  render() {
+    return (
+      <section>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
